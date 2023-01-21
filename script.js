07ef676cb1f3b0d72ccab64c2e50fdeb7aac4a7e
Dustin Kelley <kelleydustin2018@gmail.com>
2023-01-21 14:37:46 -0600

    finished project

---
+++ script.js
@@ -0,0 +1,74 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+const count = 30;
+const apiKey = 'V93dkLVrY0-BFVfAIs0IhTDOZZo3mq7lwUnZGJm9wA0';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+    imagesLoaded++;
+    if(imageLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
+//Helper Function
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    photosArray.forEach((photo) => {
+      const item = document.createElement('a');
+   
+      setAttributes(item, {
+        href: photo.links.html,
+        target: '_blank',
+      })
+      const img = document.createElement('img');
+    
+      setAttributes(img, {
+        src: photo.urls.regular,
+        alt: photo.alt_description,
+        title: photo.alt_description,
+      });
+      img.addEventListener('load', imageLoaded);
+      item.appendChild(img);
+      imageContainer.appendChild(item);
+
+    });
+}
+
+
+
+//Get photos from unsplash api
+async function getPhotos() {
+    try {
+      const response = await fetch(apiUrl);
+      photosArray = await response.json();
+      displayPhotos();
+    } catch (error) {
+
+    }
+};
+
+//check to see if scrolling near bottom of page
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + 
+    window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+      ready = false;
+      getPhotos();
+  }
+})
+
+getPhotos();
